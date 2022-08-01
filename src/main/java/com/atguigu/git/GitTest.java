5b9416eaafba9f1609fefbd9eee0235a11bc212b
yzw <1023588994@qq.com>
2022-08-01 20:18:54 +0800

    第一次更新

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,14 +1,9 @@
 package com.atguigu.git;
 
-/**
- * title
- *
- * @auther yzw
- * @date 2022/8/1
- * description:
- */
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("world");
     }
 }

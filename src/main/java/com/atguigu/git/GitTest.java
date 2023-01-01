ed3b2de52a3b87134add0db741ee50ae2fbd9370
yzw <1023588994@qq.com>
2023-01-01 11:30:07 +0800

    主分支添加内容

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("测试上传到远程库");
         System.out.println("测试远程库pull下拉");
         System.out.println("测试上传到git");
+        System.out.println("主分支添加内容");
     }
 }
